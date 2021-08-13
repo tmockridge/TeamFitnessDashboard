474cfdfdc7fbdf44ab178ed32bf1ca5e3144cc67
tmockridge <tmockri@clemson.edu>
2021-08-13 18:37:13 -0400

    main script

--- app.py
+++ app.py
@@ -1,18 +1,3 @@
-import pandas as pd
-# from sqlalchemy import create_engine
-# import psycopg2
+import scraper
+import db_update
 
-maindf = pd.read_json("data1.json")
-print(maindf.info())
-
-pages = [2,3,4,5,6,7,8]
-for page in pages:
-    print(f"loadingpage{page}")
-
-    df = pd.read_json(f"data{page}.json")
-    # print(df.info())
-    maindf = maindf.append(df)
-
-    print(maindf.info())
-
-maindf.to_csv('data.csv')
